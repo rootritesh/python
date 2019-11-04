7685c16d40f44f80ce7e855173d7d4b899115594
rootritesh <57373623+rootritesh@users.noreply.github.com>
2019-11-05 03:11:14 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+#create the dictionary and take input form the user and return the meaning of the word from the dictionary
+d={"":"www.google.com","facebook":"www.facebook.com","youtube":"www.youtube.com"}
+key=input("enter the name")
+print(d[key])
